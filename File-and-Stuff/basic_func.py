b71b33b94e8087b6a83d3e4d0a097fb1a55f4f05
Andy <673536904@qq.com>
2014-12-31 17:40:31 -0500

    Update basic_func.py

--- File-and-Stuff/basic_func.py
+++ File-and-Stuff/basic_func.py
@@ -1,15 +1,23 @@
 def add_something():
+    file_name=input("what is the name of the file? ")
+    f=open(file_name,'a')
     to_be_add=input("what do you want to add? ")
-    f=open('test.txt','a')
     f.write(to_be_add)
     f.close()
 
-def show():
-    f=open('test.txt')
-    for line in f.readlines():
-        print(line)
+def new_file():
+    file_name=input("what is the name of the file? ")
+    f=open(file_name,'w')
+    f.close()
+    show(file_name)
     
 
+def show(file_name):
+    f=open(file_name)
+    print(f.read())
+#for line in f.readlines():
+#        print(line)
+#    print(f.read())    
 
-#add_something()
-show()
+new_file()
+add_something()
